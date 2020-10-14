6479e56a27cf5fe96f759866fd517db5c49c4a28
unknown <a1282630196@qq.com>
2020-10-14 22:25:11 +0800

    aaa

--- src/main/java/com/ecs/mapper/UserMapper.java
+++ src/main/java/com/ecs/mapper/UserMapper.java
@@ -32,15 +32,6 @@ public interface UserMapper {
     @Update("UPDATE user SET token_create_at=#{tokenCreateTime} WHERE user_id =#{userId};")
     void updateTokenCreateTimeByUserId(@Param("tokenCreateTime") String tokenCreateTime, @Param("userId") String userId);
 
-    @Update("UPDATE user SET prisoner_id = #{prisonerId} WHERE id = #{id};")       //prisonerId修改
-    void updatePrisonerIdById(@Param("prisonerId") String prisonerId, @Param("id") String id);
-
-
-    @Update("UPDATE user SET username=#{userName} WHERE id =#{id};")
-    void updateUserNameById(@Param("userName") String userName, @Param("id") String id);
-
-    @Update("UPDATE user SET password=#{password} WHERE id =#{id};")
-    void updatePasswordById(@Param("password") String password, @Param("id") String id);
 
     @Update("UPDATE user SET phone=#{phone} WHERE id =#{id};")
     void updatePhoneById(@Param("phone") String phone, @Param("id") String id);
@@ -49,7 +40,7 @@ public interface UserMapper {
     void deleteById(@Param("id") String id);
 
 
-    @Select("SELECT id FROM user WHERE username=#{userName};")
+    @Select("SELECT user_id FROM user WHERE user_name=#{userName};")
     String getUserIdByUserName(@Param("userName") String userName);
 
     @Select("SELECT * FROM user;")
