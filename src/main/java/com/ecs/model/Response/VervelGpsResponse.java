5133876d1cbfea96f086d27e39dbadfa90cd2f4a
guoxidong <a1282630196@qq.com>
2020-10-27 16:38:48 +0800

    aaa

---
+++ src/main/java/com/ecs/model/Response/VervelGpsResponse.java
@@ -0,0 +1,55 @@
+package com.ecs.model.Response;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class VervelGpsResponse {
+    private String longitude;
+
+    private String latitude;
+
+    private String createAt;
+
+    private String prisonerId;
+
+    private String prisonerName;
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getPrisonerId() {
+        return prisonerId;
+    }
+
+    public void setPrisonerId(String prisonerId) {
+        this.prisonerId = prisonerId;
+    }
+
+    public String getPrisonerName() {
+        return prisonerName;
+    }
+
+    public void setPrisonerName(String prisonerName) {
+        this.prisonerName = prisonerName;
+    }
+}
--- src/main/java/com/ecs/server/FrontSchedule.java
+++ src/main/java/com/ecs/server/FrontSchedule.java
@@ -1,9 +1,10 @@
 package com.ecs.server;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ecs.mapper.VervelGpsMapper;
+import com.ecs.mapper.*;
 import com.ecs.model.*;
 import com.ecs.model.Response.AbnormalResponse;
+import com.ecs.model.Response.VervelGpsResponse;
 import com.ecs.service.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,11 +21,20 @@ import java.util.Map;
 public class FrontSchedule {
     private TestNetworksService testNetworksService;
     private VervelGpsMapper vervelGpsMapper;
+    private VervelMapper vervelMapper;
+    private DeviceMapper deviceMapper;
+    private ConvoyMapper convoyMapper;
+    private PrisonerMapper prisonerMapper;
     private static final int timestamp = 2000;
 
-    public FrontSchedule(TestNetworksService testNetworksService, VervelGpsMapper vervelGpsMapper) {
+    public FrontSchedule(TestNetworksService testNetworksService, VervelGpsMapper vervelGpsMapper, VervelMapper vervelMapper,
+                         DeviceMapper deviceMapper, ConvoyMapper convoyMapper, PrisonerMapper prisonerMapper) {
         this.testNetworksService = testNetworksService;
         this.vervelGpsMapper = vervelGpsMapper;
+        this.vervelMapper = vervelMapper;
+        this.deviceMapper = deviceMapper;
+        this.convoyMapper = convoyMapper;
+        this.prisonerMapper = prisonerMapper;
     }
 
     //@Scheduled(fixedRate = timestamp)
@@ -48,9 +58,26 @@ public class FrontSchedule {
         Map<String, Object> maps = new HashMap<>();
         if (FrontServer.getOnlineCount() > 0){
             JSONObject json = new JSONObject();
+            List<VervelGps> vervelGpsList = vervelGpsMapper.getAll();
+            List<VervelGpsResponse> vervelGpsResponseList = new ArrayList<>();
+            for(int i=0;i<vervelGpsList.size();i++){
+                VervelGps vervelGps = vervelGpsList.get(i);
+                VervelGpsResponse vervelGpsResponse = new VervelGpsResponse();
+                String deviceNo = vervelMapper.getDeviceNoByVervelNo(vervelGps.getVervelNo());
+                String userId = deviceMapper.getByDeviceNo(deviceNo).getUserId();
+                String prisonerId = convoyMapper.getPrisonerIdByUserId(userId);
+                String prisonerName = prisonerMapper.getPrisonerNameByPrisonerId(prisonerId);
+
+                vervelGpsResponse.setLongitude(vervelGps.getLongitude());
+                vervelGpsResponse.setLatitude(vervelGps.getLatitude());
+                vervelGpsResponse.setCreateAt(vervelGps.getCreateAt());
+                vervelGpsResponse.setPrisonerId(prisonerId);
+                vervelGpsResponse.setPrisonerName(prisonerName);
+                vervelGpsResponseList.add(vervelGpsResponse);
+            }
             maps.put("type", "vervelGps");
             maps.put("code",200);
-            maps.put("data",vervelGpsMapper.getAll());
+            maps.put("data",vervelGpsResponseList);
             FrontServer.sendInfo(maps);
         }
     }
